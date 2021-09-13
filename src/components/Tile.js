2fe1d515a2dadc506ad9fe7575412e534b0b9f5a
LeeAnn Grapes <leeann.grapes@gmail.com>
2021-09-12 21:31:52 -0600

    Updated overall page and card styling

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { Card, Row } from "react-bootstrap";
+import { Card, Row, Col } from "react-bootstrap";
 import styles from "./tile.css";
 
 function Tile(props) {
   return (
-    <Card className="d-flex mx-auto p-5 tile">
+    <Card className="d-flex mx-1 pt-2 tile">
       <Card.Title>
         <p>{props.day}</p>
       </Card.Title>
       <Card.Body>
         <div className="d-flex flex-row justify-content-center">
-          <p>{props.maxTemp}</p>|<p>{props.minTemp}</p>
+          <p>{props.maxTemp}° </p>|<p> {props.minTemp}°</p>
         </div>
         <p>{props.overview}</p>
       </Card.Body>
